6f1489fd9f49dad8af19cc20a162fea9b1f0ab6a
ExtremeCodeTV <extremecodetv@gmail.com>
2017-08-21 12:34:19 +0700

    Remove unused namespaces

--- src/SocksSharp/Proxy/IProxyClient.cs
+++ src/SocksSharp/Proxy/IProxyClient.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net.Sockets;
 
 namespace SocksSharp.Proxy
 {
