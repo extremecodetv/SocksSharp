776f5aba42cb2b0953a479dcbd04edd96dc80476
ExtremeCodeTV <extremecodetv@gmail.com>
2017-08-23 16:57:34 +0700

    Docs for IProxy

--- src/SocksSharp/Proxy/IProxy.cs
+++ src/SocksSharp/Proxy/IProxy.cs
@@ -7,10 +7,23 @@ using System.Threading.Tasks;
 
 namespace SocksSharp.Proxy
 {
+    /// <summary>
+    /// Provides an interface for proxy client 
+    /// </summary>
     public interface IProxy
     {
+        /// <summary>
+        /// Gets or sets proxy settings
+        /// </summary>
         IProxySettings Settings { get; set; }
 
+        /// <summary>
+        /// Create connection to destination host via proxy server.
+        /// </summary>
+        /// <param name="destinationHost">Host</param>
+        /// <param name="destinationPort">Port</param>
+        /// <param name="tcpClient">Connection with proxy server.</param>
+        /// <returns>Connection to destination host</returns>
         TcpClient CreateConnection(string destinationHost, int destinationPort, TcpClient client);
     }
 }
