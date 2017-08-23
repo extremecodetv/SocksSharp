d325dfdb54a019c52b9758a3dac74ea5eb3447b4
ExtremeCodeTV <extremecodetv@gmail.com>
2017-08-23 17:23:05 +0700

    Refactor

--- src/SocksSharp/Proxy/ProxyClient.cs
+++ src/SocksSharp/Proxy/ProxyClient.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Sockets;
 using System.Security;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace SocksSharp.Proxy
 {
--- src/SocksSharp/Proxy/Response/IResponseBuilder.cs
+++ src/SocksSharp/Proxy/Response/IResponseBuilder.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net.Http;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
