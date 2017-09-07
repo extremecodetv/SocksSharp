dc05a4c804384970ee7696ae9212841b26114942
ExtremeCodeTV <extremecodetv@gmail.com>
2017-09-07 21:21:22 +0700

    Add GatherTestConfiguration method

--- tests/SocksSharp.Tests/ProxyClientTests.cs
+++ tests/SocksSharp.Tests/ProxyClientTests.cs
@@ -1,10 +1,21 @@
 using System;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace SocksSharp.Tests
 {
     public class ProxyClientTests
     {
+
+        private void GatherTestConfiguration()
+        {
+            var appConfigMsgWarning = "{0} not configured in proxysettings.json! Some tests may fail.";
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("proxysettings.json")
+                .Build();
+        }
+
         [Fact]
         public void Test1()
         {
