8a16544ff49f784e574f8fc0a83a9757df47e7fb
ExtremeCodeTV <extremecodetv@gmail.com>
2017-09-08 15:33:35 +0700

    Implement tests

--- tests/SocksSharp.Tests/ProxyClientTests.cs
+++ tests/SocksSharp.Tests/ProxyClientTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Net.Http;
 using Microsoft.Extensions.Configuration;
@@ -10,12 +10,219 @@ using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace SocksSharp.Tests
 {
     public class ProxyClientTests
     {
         private ProxySettings proxySettings;
+        
+        public ProxyClientTests()
+        {
+            GatherTestConfiguration();
+        }
+
+        #region Tests
+
+        [Fact]
+        public async Task RequestHeadersTest()
+        {
+            EnsureIsConfigured();
+
+            var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri("http://httpbin.org/user-agent");
+            message.Headers.Add("User-Agent", userAgent);
+
+            var response = await GetResponseMessage(message);
+
+            Assert.NotNull(response);
+
+            var userAgentActual = await GetJsonStringValue(response, "user-agent");
+
+            Assert.NotEmpty(userAgentActual);
+            Assert.Equal(userAgent, userAgentActual);
+        }
+
+        [Fact]
+        public async Task GetRequestTest()
+        {
+            EnsureIsConfigured();
+
+            var key = "key";
+            var value = "value";
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri($"http://httpbin.org/get?{key}={value}");
+
+            var response = await GetResponseMessage(message);
+
+            var actual = await GetJsonDictionaryValue(response, "args");
+
+            Assert.True(actual.ContainsKey(key));
+            Assert.True(actual.ContainsValue(value));
+        }
+
+        [Fact]
+        public async Task GetUtf8Test()
+        {
+            EnsureIsConfigured();
+
+            var excepted = "∮";
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri("http://httpbin.org/encoding/utf8");
+
+
+            var response = await GetResponseMessage(message);
+            var actual = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains(excepted, actual);
+        }
+
+        [Fact]
+        public async Task GetHtmlPageTest()
+        {
+            EnsureIsConfigured();
+
+            long exceptedLength = 3741;
+            var contentType = "text/html";
+            var charSet = "utf-8";
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri("http://httpbin.org/html");
+            
+            var response = await GetResponseMessage(message);
+
+            var content = response.Content;
+            Assert.NotNull(content);
+
+            var headers = response.Content.Headers;
+            Assert.NotNull(headers);
+
+            Assert.NotNull(headers.ContentLength);
+            Assert.Equal(exceptedLength, headers.ContentLength.Value);
+            Assert.NotNull(headers.ContentType);
+            Assert.Equal(contentType, headers.ContentType.MediaType);
+            Assert.Equal(charSet, headers.ContentType.CharSet);
+        }
+
+        [Fact]
+        public async Task DelayTest()
+        {
+            EnsureIsConfigured();
+            
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri("http://httpbin.org/delay/4");
+
+            var response = await GetResponseMessage(message);
+            var source = response.Content.ReadAsStringAsync();
+
+            Assert.NotNull(response);
+            Assert.NotNull(source);
+        }
+
+        [Fact]
+        public async Task StreamTest()
+        {
+            EnsureIsConfigured();
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri("http://httpbin.org/stream/20");
+
+            var response = await GetResponseMessage(message);
+            var source = response.Content.ReadAsStringAsync();
+
+            Assert.NotNull(response);
+            Assert.NotNull(source);
+        }
+
+        [Fact]
+        public async Task GzipTest()
+        {
+            EnsureIsConfigured();
+
+            var excepted = "gzip, deflate";
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri("http://httpbin.org/gzip");
+
+            var response = await GetResponseMessage(message);
+            var acutal = await GetJsonStringValue(response, "Accept-Encoding");
+
+            Assert.NotNull(response);
+            Assert.NotNull(acutal);
+            Assert.Equal(excepted, acutal);
+        }
+
+        [Fact]
+        public async Task CookiesTest()
+        {
+            EnsureIsConfigured();
+            
+            HttpResponseMessage response = null; 
+
+            var name = "name";
+            var value = "value";
+
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri($"http://httpbin.org/cookies/set?{name}={value}");
+
+            var handler = CreateNewSocks5Client();
+            handler.CookieContainer = new System.Net.CookieContainer();
+            var client = new HttpClient(handler);
+
+            try
+            {
+                response = await client.SendAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception caught! " + ex.Message);
+            }
+
+            Assert.NotNull(response);
+            var cookies = handler.CookieContainer.GetCookies(new Uri("http://httpbin.org/"));
+
+            Assert.Equal(1, cookies.Count);
+            var cookie = cookies[name];
+            Assert.Equal(name, cookie.Name);
+            Assert.Equal(value, cookie.Value);
+
+            handler.Dispose();
+            client.Dispose();
+        }
+
+        [Fact]
+        public async Task StatusCodeTest()
+        {
+            EnsureIsConfigured();
+
+            var code = "404";
+            var excepted = "NotFound";
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri($"http://httpbin.org/status/{code}");
+
+            var response = await GetResponseMessage(message);
+
+            Assert.NotNull(response);
+            Assert.Equal(excepted, response.StatusCode.ToString());
+        }
+
+        #endregion
+
+        #region Helpers
 
         private void GatherTestConfiguration()
         {
@@ -30,7 +237,7 @@ namespace SocksSharp.Tests
                                 .AddJsonFile("proxysettings.json")
                                 .Build();
             }
-            catch(FileNotFoundException)
+            catch (FileNotFoundException)
             {
                 Debug.WriteLine("proxysettings.json not found in project folder");
                 return;
@@ -68,35 +275,36 @@ namespace SocksSharp.Tests
             return new ProxyClientHandler<Socks5>(proxySettings);
         }
 
-        public ProxyClientTests()
+        private async Task<string> GetJsonStringValue(HttpResponseMessage response, string valueName)
         {
-            GatherTestConfiguration();
-        }
-
-        [Fact]
-        public virtual async Task RequestHeadersTest()
-        {
-            CheckIsConfigured();
-
-            var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
-
-            var message = new HttpRequestMessage();
-            message.Method = HttpMethod.Get;
-            message.RequestUri = new Uri("http://httpbin.org/user-agent");
-            message.Headers.Add("User-Agent", userAgent);
-
-            var response = await GetResponseMessage(message);
-
-            Assert.NotNull(response);
-
-            var obj = await Deserialize(response);
-            Assert.Equal(obj.user-agent, userAgent);
-        }
-
-        private async Task<dynamic> Deserialize(HttpResponseMessage response)
-        {
+            JToken token;
             var source = await response.Content.ReadAsStringAsync();
-            return JObject.Parse(source);
+            var obj = JObject.Parse(source);
+
+            var result = obj.TryGetValue(valueName, out token);
+
+            if (!result)
+            {
+                return String.Empty;
+            }
+
+            return token.Value<string>();
+        }
+
+        private async Task<Dictionary<string, string>> GetJsonDictionaryValue(HttpResponseMessage response, string valueName)
+        {
+            JToken token;
+            var source = await response.Content.ReadAsStringAsync();
+            var obj = JObject.Parse(source);
+
+            var result = obj.TryGetValue(valueName, out token);
+
+            if (!result)
+            {
+                return null;
+            }
+
+            return token.ToObject<Dictionary<string, string>>();
         }
 
         private async Task<HttpResponseMessage> GetResponseMessage(HttpRequestMessage requestMessage)
@@ -121,7 +329,7 @@ namespace SocksSharp.Tests
             return response;
         }
 
-        private void CheckIsConfigured()
+        private void EnsureIsConfigured()
         {
             if (proxySettings == null 
                 || proxySettings.Host == null 
@@ -130,5 +338,7 @@ namespace SocksSharp.Tests
                 throw new Exception("Please add your proxy settings to proxysettings.json in build folder");
             }            
         }
+
+        #endregion
     }
 }
